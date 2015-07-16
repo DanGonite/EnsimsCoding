ce80f50182634dd4b2646d634361dd76eba72385
DanGonite <danzinc57@gmail.com>
2015-07-16 17:31:02 +0100

    Added the stuff that you wanted

--- SimStats.py
+++ SimStats.py
@@ -9,53 +9,57 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import csv, math
+import csv, math, sys
 
-minlist = ["Min", ""]
-maxlist = ["Max", ""]
-meanlist = ["Mean", ""]
-stddevlist = ["Standard Deviation", ""]
-headlist = []
+##ifile  = sys.argv[1]
+##ofile = sys.argv[2]
 
-with open('run\SimResults.csv', 'rb') as csvfile:   # Opens file as "read-able binary"
-    reader = list(csv.reader(csvfile))  # Reads the file as a list
-    for item in reader[0]:  # Loops through items in the first row (header) of the file
-        headlist.append(item)   # Creates the Header row
-	y = len(headlist)  # Calculates the number of columns
-    for n in range(y):  # Loops through columns in the file
-        value = []  # Resets "Value" list to empty
-        for row in reader:  # Loops through rows in the file
+def getSimStats(ifile, ofile):
+    minlist = ["Min", ""]
+    maxlist = ["Max", ""]
+    meanlist = ["Mean", ""]
+    stddevlist = ["Standard Deviation", ""]
+    headlist = []
+
+    with open(ifile, 'rb') as csvfile:   # Opens file as "read-able binary"
+        reader = list(csv.reader(csvfile))  # Reads the file as a list
+        for item in reader[0]:  # Loops through items in the first row (header) of the file
+            headlist.append(item)   # Creates the Header row
+    	y = len(headlist)  # Calculates the number of columns
+        for n in range(y):  # Loops through columns in the file
+            value = []  # Resets "Value" list to empty
+            for row in reader:  # Loops through rows in the file
+                try:
+                    data = float(row[n])    # Sets "data" to the value of column "n" in the row as a decimal
+                    value.append(data)  # Adds "data" to the list of "Values"
+                except ValueError:  # Checks if an error has been returned
+                    pass
             try:
-                data = float(row[n])    # Sets "data" to the value of column "n" in the row as a decimal
-                value.append(data)  # Adds "data" to the list of "Values"
-            except ValueError:  # Checks if an error has been returned
+                minlist.append(min(value))  # Calculates the minimum value in the "value" list and adds it to "minlist"
+                maxlist.append(max(value))  # Calculates the maximum value in the "value" list and adds it to "maxlist"
+                mean = 0
+                for item in value:
+                    mean += item
+                mean /= len(value)   # Adds each item in the "value" list to "mean" and divides "mean" by the number of values that were added
+                meanlist.append(mean)   # Adds "mean" to the list of means
+                sumsq = 0
+                for i in range(len(value)):
+                	sumsq += (value[i] - mean) **2
+                stddevlist.append(math.sqrt(sumsq/(len(value)-1)))
+            except ValueError:  # Checks for an error
                 pass
-        try:
-            minlist.append(min(value))  # Calculates the minimum value in the "value" list and adds it to "minlist"
-            maxlist.append(max(value))  # Calculates the maximum value in the "value" list and adds it to "maxlist"
-            mean = 0
-            for item in value:
-                mean += item
-            mean /= len(value)   # Adds each item in the "value" list to "mean" and divides "mean" by the number of values that were added
-            meanlist.append(mean)   # Adds "mean" to the list of means
-            sumsq = 0
-            for i in range(len(value)):
-            	sumsq += (value[i] - mean) **2
-            stddevlist.append(math.sqrt(sumsq/(len(value)-1)))
-        except ValueError:  # Checks for an error
-            pass
-    minlist[2] = "" # Removes the 3rd entry (Job_Id)
-    maxlist[2] = "" # Removes the 3rd entry (Job_Id)
-    meanlist[2] = ""    # Removes the 3rd entry (Job_Id)
-    stddevlist[2] = ""    # Removes the 3rd entry (Job_Id)
+        minlist[2] = "" # Removes the 3rd entry (Job_Id)
+        maxlist[2] = "" # Removes the 3rd entry (Job_Id)
+        meanlist[2] = ""    # Removes the 3rd entry (Job_Id)
+        stddevlist[2] = ""    # Removes the 3rd entry (Job_Id)
 
-with open('run\SimStats.csv', "wb") as csvfile:     # Opens file as "write-able" binary
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerow(headlist)   # }
-    writer.writerow(minlist)    # }
-    writer.writerow(maxlist)    # } Writes the data to a different document
-    writer.writerow(meanlist)   # }
-    writer.writerow(stddevlist) # }
+    with open(ofile, "wb") as csvfile:     # Opens file as "write-able" binary
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(headlist)   # }
+        writer.writerow(minlist)    # }
+        writer.writerow(maxlist)    # } Writes the data to a different document
+        writer.writerow(meanlist)   # }
+        writer.writerow(stddevlist) # }
 
-
+getSimStats("run/SimResults.csv","run/SimStats.csv")
 

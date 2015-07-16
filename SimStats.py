acb0064adf061655b7338774c60ada98e7e9fe35
DanGonite <danzinc57@gmail.com>
2015-07-16 16:15:42 +0100

    Added comments (delete/change as necessary)

--- SimStats.py
+++ SimStats.py
@@ -16,39 +16,39 @@ maxlist = ["Max", ""]
 meanlist = ["Mean", ""]
 headlist = []
 
-with open('run\SimResults.csv', 'rb') as csvfile:
-    reader = list(csv.reader(csvfile))
-    for item in reader[0]:
-        headlist.append(item)
-	y = len(headlist)
-    for n in range(y):
-        value = []
-        for row in reader:
+with open('run\SimResults.csv', 'rb') as csvfile:   # Opens file as "read-able binary"
+    reader = list(csv.reader(csvfile))  # Reads the file as a list
+    for item in reader[0]:  # Loops through items in the first row (header) of the file
+        headlist.append(item)   # Creates the Header row
+	y = len(headlist)  # Calculates the number of columns
+    for n in range(y):  # Loops through columns in the file
+        value = []  # Resets "Value" list to empty
+        for row in reader:  # Loops through rows in the file
             try:
-                data = float(row[n])
-                value.append(data)
-            except ValueError:
+                data = float(row[n])    # Sets "data" to the value of column "n" in the row as a decimal
+                value.append(data)  # Adds "data" to the list of "Values"
+            except ValueError:  # Checks if an error has been returned
                 pass
         try:
-            minlist.append(min(value))
-            maxlist.append(max(value))
-            mean = 0
-            for item in value:
-                mean += item
-            mean /= len(value)
-            meanlist.append(mean)
-        except ValueError:
+            minlist.append(min(value))  # Calculates the minimum value in the "value" list and adds it to "minlist"
+            maxlist.append(max(value))  # Calculates the maximum value in the "value" list and adds it to "maxlist"
+            mean = 0    # Sets "mean" as 0
+            for item in value:  # Loops through the "value" list
+                mean += item    # Adds each item in the "value" list to "mean"
+            mean /= len(value)  # Divides "mean" by the number of values that were added
+            meanlist.append(mean)   # Adds "mean" to the list of means
+        except ValueError:  # Checks for an error
             pass
-    minlist[2] = ""
-    maxlist[2] = ""
-    meanlist[2] = ""
+    minlist[2] = "" # Removes the 3rd entry (Job_Id)
+    maxlist[2] = "" # Removes the 3rd entry (Job_Id)
+    meanlist[2] = ""    # Removes the 3rd entry (Job_Id)
 
-with open('run\SimStats.csv', "wb") as csvfile:
+with open('run\SimStats.csv', "wb") as csvfile:     # Opens file as "write-able" binary
     writer = csv.writer(csvfile, delimiter=",")
-    writer.writerow(headlist)
-    writer.writerow(minlist)
-    writer.writerow(maxlist)
-    writer.writerow(meanlist)
+    writer.writerow(headlist)   # }
+    writer.writerow(minlist)    # } Writes the data
+    writer.writerow(maxlist)    # } to a different document
+    writer.writerow(meanlist)   # }
 
 
 

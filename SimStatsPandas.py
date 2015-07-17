f10a0bc8930a519940ead3303d23eaa97a972a22
DanGonite <danzinc57@gmail.com>
2015-07-17 16:23:46 +0100

    Removed # column

--- SimStatsPandas.py
+++ SimStatsPandas.py
@@ -2,6 +2,13 @@ import pandas as pd, sys
 
 def getSimStats(ifile, ofile):
     df = pd.read_csv(ifile)
+    col = []
+    for column in df:
+        if column!= "#":
+            col.append(column)
+    df = df[col]
+    print df
     df.describe().to_csv(ofile)
 
-getSimStats(sys.argv[1], sys.argv[2])
+##getSimStats(sys.argv[1], sys.argv[2])
+getSimStats("run//SimResults.csv", "run//SimStatsPandas.csv")

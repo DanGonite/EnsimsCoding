13982d1e044bd1d8d8d42955c03825d84eac5096
DanGonite <danzinc57@gmail.com>
2015-07-17 15:36:01 +0100

    Fixed and removed licensce-y stuff

--- SimStatsPandas.py
+++ SimStatsPandas.py
@@ -1,15 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module2
-# Purpose:
-#
-# Author:      Jurong
-#
-# Created:     17/07/2015
-# Copyright:   (c) Jurong 2015
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
-import pandas as pd
+import pandas as pd, sys
 
 def getSimStats(ifile, ofile):
     df = pd.read_csv(ifile)

98caadb216979e53b05550f2fa58e824659e9a12
DanGonite <danzinc57@gmail.com>
2015-07-17 15:40:27 +0100

    Shortened

--- SimStatsPandas.py
+++ SimStatsPandas.py
@@ -4,5 +4,4 @@ def getSimStats(ifile, ofile):
     df = pd.read_csv(ifile)
     df.describe().to_csv(ofile)
 
-##getSimStats(sys.argv[1], sys.argv[2])
-getSimStats("run//SimResults.csv", "run//SimStatsPandas.csv")
+getSimStats(sys.argv[1], sys.argv[2])

4e117896cd0380d1adc7fd5266584dfee2e97a6f
DanGonite <danzinc57@gmail.com>
2015-07-20 18:15:18 +0100

    Mad

--- SimStatsPandas.py
+++ SimStatsPandas.py
@@ -2,8 +2,11 @@ import pandas as pd, sys
 
 def getSimStats(ifile, ofile):
     df = pd.read_csv(ifile).describe().drop('#', 1)
-    df.insert(0, "Time/Date", "")
-    df.insert(0, "Job_ID", "")
+    # This
+##    df.insert(0, "Time/Date", "")
+##    df.insert(0, "Job_ID", "")
+    # Or this
+    df.insert(0, "Time/Date", ""); df.insert(0, "Job_ID", "")
     df.to_csv(ofile)
 
 getSimStats(sys.argv[1], sys.argv[2])

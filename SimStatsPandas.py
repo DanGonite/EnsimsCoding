2c8d4856c9277e48af3140433fd6955fd4f8d2b5
DanGonite <danzinc57@gmail.com>
2015-07-20 17:16:53 +0100

    Modified the .describe() and the format

--- SimStatsPandas.py
+++ SimStatsPandas.py
@@ -6,7 +6,15 @@ def getSimStats(ifile, ofile):
     for column in df:
         if column!= "#":
             col.append(column)
-    df = df[col]
-    df.describe().to_csv(ofile)
+    df = df[col].describe().transpose()
+    col = []
+    for stat in df:
+        if stat == "mean" or stat == "min" or stat == "max" or stat == "std":
+            col.append(stat)
+    df = df[col].transpose()
+    df.insert(0, "Time/Date", "")
+    df.insert(0, "Job_ID", "")
+    df.to_csv(ofile)
 
-getSimStats(sys.argv[1], sys.argv[2])
+##getSimStats(sys.argv[1], sys.argv[2])
+getSimStats("run/SimResults.csv", "run/SimStatsPandas.csv")

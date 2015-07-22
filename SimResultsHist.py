373da7328a38951ba84923ab84c6e8b89d49ec9a
DanGonite <danzinc57@gmail.com>
2015-07-22 11:02:46 +0100

    Added command line compatibility

--- SimResultsHist.py
+++ SimResultsHist.py
@@ -1,29 +1,32 @@
-import matplotlib.pyplot as plt, pandas as pd
+import matplotlib.pyplot as plt, pandas as pd, sys
 
-plt.close("all")
-df = pd.read_csv("run/SimResults.csv")
-df = df.ix[:, 3:]
-dflist = pd.DataFrame.to_dict(df)
-i = 0
-maxlist = []
-for item in dflist:
-    for val in dflist[item]:
-        key = dflist[item]
-        try:
-            maxlist.append(float(key[val]))
-        except ValueError:
-            pass
-maxi = max(maxlist)
-for column in df.columns:
-    plt.subplot(4,4,i+1)
-    x = []
-    x.append(df[[column]])
-    y = 0 * len(x)
-    plt.hist(x)
-    plt.axis([0, maxi, 0, 50])
-    plt.xlabel('Hours')
-    plt.ylabel('Frequency')
-    plt.title(column)
-    plt.grid(True)
-    i += 1
-plt.show()
+def Hist(ifile):
+    plt.close("all")
+    df = pd.read_csv("run/SimResults.csv")
+    df = df.ix[:, 3:]
+    dflist = pd.DataFrame.to_dict(df)
+    i = 0
+    maxlist = []
+    for item in dflist:
+        for val in dflist[item]:
+            key = dflist[item]
+            try:
+                maxlist.append(float(key[val]))
+            except ValueError:
+                pass
+    maxi = max(maxlist)
+    for column in df.columns:
+        plt.subplot(4,4,i+1)
+        x = []
+        x.append(df[[column]])
+        y = 0 * len(x)
+        plt.hist(x)
+        plt.axis([0, maxi, 0, 50])
+        plt.xlabel('Hours')
+        plt.ylabel('Frequency')
+        plt.title(column)
+        plt.grid(True)
+        i += 1
+    plt.show()
+
+Hist(sys.argv[1])

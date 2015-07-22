0a1096c4269f4cac0596b9e83a9e78cce7470aee
DanGonite <danzinc57@gmail.com>
2015-07-22 11:16:52 +0100

    Made it slightly shorter

--- SimResultsHist.py
+++ SimResultsHist.py
@@ -2,26 +2,22 @@ import matplotlib.pyplot as plt, pandas as pd, sys
 
 def Hist(ifile):
     plt.close("all")
-    df = pd.read_csv(ifile)
-    df = df.ix[:, 3:]
+    df = pd.read_csv(ifile).ix[:, 3:]
     dflist = pd.DataFrame.to_dict(df)
     i = 0
     maxlist = []
     for item in dflist:
         for val in dflist[item]:
-            key = dflist[item]
             try:
-                maxlist.append(float(key[val]))
+                maxlist.append(float(dflist[item][val]))
             except ValueError:
                 pass
-    maxi = max(maxlist)
     for column in df.columns:
         plt.subplot(4,4,i+1)
         x = []
         x.append(df[[column]])
-        y = 0 * len(x)
         plt.hist(x)
-        plt.axis([0, maxi, 0, 50])
+        plt.axis([0, max(maxlist), 0, 50])
         plt.xlabel('Hours')
         plt.ylabel('Frequency')
         plt.title(column)

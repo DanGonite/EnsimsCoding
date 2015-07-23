ff34609fb4926c04ef851028a0d42361f3226af8
DanGonite <danzinc57@gmail.com>
2015-07-23 10:36:46 +0100

    Made it

---
+++ SimResultsHistPy3.py
@@ -0,0 +1,9 @@
+import pandas as pd, matplotlib.pyplot as plt, numpy as np, math, sys
+
+def Hist(ifile):
+    df = pd.read_csv(ifile).ix[:, 3:]
+    size=math.floor(math.sqrt(len(df.columns)))
+    df.hist(color="k", alpha=0.5, bins=np.arange(0,(size*1000),200), normed=False, sharex=True, sharey=True, figsize=(14.2,8), layout=(size,size))
+    plt.show()
+
+Hist(sys.argv[1])

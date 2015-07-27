21f0fdf521dc4b04f558e58b5caaff07d2fbf901
DanGonite <danzinc57@gmail.com>
2015-07-27 20:27:40 +0100

    Added sys functioning

--- SimResultsHistPy3Extras.py
+++ SimResultsHistPy3Extras.py
@@ -1,18 +1,21 @@
-import numpy as np, matplotlib.pyplot as plt, pandas as pd, math
+import numpy as np, matplotlib.pyplot as plt, pandas as pd, math, sys
 
-df = pd.read_csv("C:/Users/Jurong/EnsimsCoding/run/SimResults.csv").ix[:, 3:]
-size = math.floor(math.sqrt(len(df.columns)))
-x = 0
-fig, axes = plt.subplots(nrows=size, ncols=size, sharey=True)
-figure, axs = plt.subplots(nrows=1, ncols=1, sharey=True)
+def Hist(ifile, ofolder):
+    df = pd.read_csv(ifile).ix[:, 3:]
+    size = math.floor(math.sqrt(len(df.columns)))
+    x = 0
+    fig, axes = plt.subplots(nrows=size, ncols=size, sharey=True)
+    figure, axs = plt.subplots(nrows=1, ncols=1, sharey=True)
+    
+    dflist = [col for col in df.columns]
+    numbins = np.arange(0,(size*1000)
+    
+    for ax in axes.ravel():
+        filename = dflist[x][5::]
+        ax.hist(df[dflist[x]], bins=numbins,200))
+        axs.cla()
+        axs.hist(df[dflist[x]], bins=numbins,200))
+        axs.figure.savefig(ofolder+filename+".png", format="png")
+        x += 1
 
-dflist = [col for col in df.columns]
-numbins = np.arange(0,(size*1000)
-
-for ax in axes.ravel():
-    filename = dflist[x][5::]
-    ax.hist(df[dflist[x]], bins=numbins,200))
-    axs.cla()
-    axs.hist(df[dflist[x]], bins=numbins,200))
-    axs.figure.savefig("C:/Users/Jurong/EnsimsCoding/run/"+filename+".png", format="png")
-    x += 1
+Hist(sys.argv[1], sys.argv[2])

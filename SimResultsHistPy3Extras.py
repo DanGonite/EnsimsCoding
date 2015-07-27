9dc9b8f411d4b92d589e0705f5ef25f9d7fa24a9
DanGonite <danzinc57@gmail.com>
2015-07-27 21:59:11 +0100

    Added Hours/Frequency labels

--- SimResultsHistPy3Extras.py
+++ SimResultsHistPy3Extras.py
@@ -4,18 +4,21 @@ def Hist(ifile, ofolder):
     df = pd.read_csv(ifile).ix[:, 3:]
     size = math.floor(math.sqrt(len(df.columns)))
     x = 0
-    fig, axes = plt.subplots(nrows=size, ncols=size, sharey=True)
-    figure, axs = plt.subplots(nrows=1, ncols=1, sharey=True)
+    fig, axes = plt.subplots(nrows=size, ncols=size, sharey=True, sharex = True)
+    figure, axs = plt.subplots(nrows=1, ncols=1, sharey=True, sharex = True)
     
     dflist = [col for col in df.columns]
-    numbins = np.arange(0,(size*1000)
+    numbins = np.arange(0,(size*1000),200)
     
     for ax in axes.ravel():
         filename = dflist[x][5::]
-        ax.hist(df[dflist[x]], bins=numbins,200))
+        ax.hist(df[dflist[x]], bins=numbins)
+        ax.set_xlabel("Hours")
+        ax.set_ylabel("Frequency")
         axs.cla()
-        axs.hist(df[dflist[x]], bins=numbins,200))
+        axs.hist(df[dflist[x]], bins=numbins)
         axs.figure.savefig(ofolder+filename+".png", format="png")
         x += 1
 
-Hist(sys.argv[1], sys.argv[2])
+# Hist(sys.argv[1], sys.argv[2])
+Hist("C:/Users/Jurong/EnsimsCoding/run/SimResults.csv", "C:/Users/Jurong/EnsimsCoding/run/")

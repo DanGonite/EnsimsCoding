79a55fea32dfd2d3be6f06bb4136f624a1f437ac
DanGonite <danzinc57@gmail.com>
2015-08-26 17:37:47 +0100

    I did something and I can't remember what

--- SimResultsHistPy3.py
+++ SimResultsHistPy3.py
@@ -1,9 +1,11 @@
-import pandas as pd, matplotlib.pyplot as plt, numpy as np, math, sys
+import pandas as pd, matplotlib.pyplot as plt, numpy as np, math, sys, matplotlib.pylab as lab
 
 def Hist(ifile):
     df = pd.read_csv(ifile).ix[:, 3:]
     size=math.floor(math.sqrt(len(df.columns)))
     df.hist(color="k", alpha=0.5, bins=np.arange(0,(size*1000),200), normed=False, sharex=True, sharey=True, figsize=(14.2,8), layout=(size,size))
     plt.show()
+    
 
-Hist(sys.argv[1])
+# Hist(sys.argv[1])
+Hist("C:/Users/Jurong/EnsimsCoding/run/SimResults.csv")

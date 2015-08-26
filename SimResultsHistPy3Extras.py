e14d9f20967a3847dc892341d70c46aa3331252b
DanGonite <danzinc57@gmail.com>
2015-08-26 17:35:52 +0100

    Added/Shortened titles

--- SimResultsHistPy3Extras.py
+++ SimResultsHistPy3Extras.py
@@ -7,13 +7,21 @@ def Hist(ifile, ofolder):
     dflist = [col for col in df.columns]
     x = 0
     for ax in axes.ravel():
-        filename = dflist[x][5::]   # Change as needed
+        filename = dflist[x][5::]
+        filename2 = ""
+        for letter in filename:
+            if letter == "[":
+                break
+            else:
+                filename2 += letter
         ax.hist(df[dflist[x]], bins=np.arange(0,(math.floor(math.sqrt(len(df.columns)))*1000),200))
         ax.set_xlabel("Hours"); ax.set_ylabel("Frequency")
+        ax.set_title(filename2)
         axs.cla()
         axs.hist(df[dflist[x]], bins=np.arange(0,(math.floor(math.sqrt(len(df.columns)))*1000),200))
         axs.set_xlabel("Hours"); axs.set_ylabel("Frequency")
-        axs.figure.savefig(ofolder+"/"+filename+".png", format="png")
+        axs.set_title(filename2)
+        axs.figure.savefig(ofolder+"/"+filename2+".png", format="png")
         x += 1
 
 # Hist(sys.argv[1], sys.argv[2])

279fba9c67b6ae3bceda186ec511a668b11ebd92
DanGonite <danzinc57@gmail.com>
2016-03-18 16:10:22 +0000

    Possibly works now

--- SimResultsScatterPy3v2.py
+++ SimResultsScatterPy3v2.py
@@ -1,42 +1,24 @@
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-ifile1 = "D:/USB/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv"
-ifile2 = "D:/USB/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv"
-df1 = pd.read_csv(ifile1)
-df2 = pd.read_csv(ifile2)
-dfMaster = pd.concat([df1, df2])
-dflist = [col for col in dfMaster.columns]
-f, ax = plt.subplots()
+def createScatter(ifile1, col1, ifile2, col2, ofolder):
+    df1 = pd.read_csv(ifile1)
+    df2 = pd.read_csv(ifile2)
+    f, ax = plt.subplots()
 
-col1 = "c1: InteriorEquipment:Electricity [J](RunPeriod)"
-col2 = "s1: Electricity [kWh]"
+    df3 = df1[col1]
+    df4 = df2[col2]
 
-##for item in dfMaster[col1][::100]:
-##    for itemn in dfMaster[col2][::100]:
-##        ax.scatter(item, itemn)
-##plt.show()
-##df3 = np.array([df[col1], df[col2]])
-##
-##print(df3)
+    i = 0
+    for item in df3:
+        ax.scatter(df3[i], df4[i])
+        i += 1
 
-##print(type(df2[col1].iloc[item]))
+    col1name = col1.replace(":", "")
+    col2name = col2.replace(":", "")
+    fname = col1name + " - " + col2name
+    ax.set_title(fname)
+    ax.figure.savefig(ofolder+"/"+fname+".png", format="png")
 
-print(type(df2))
+    plt.show()
 
-for item in df2[col1]:
-    print(type(item.item()))
-    print(type(df2[col1].ix(item.item())))
-    ax.scatter(item, df2[col1].ix(item))
-##df3 = pd.DataFrame(dfMaster[col1])
-##df4 = pd.DataFrame(dfMaster[col2])
-##print (type(dfMaster[col1][3]))
-##print (type(dfMaster[col1])).0
-##print (type(dfMaster))
-##print (type(df3))
-##print (type(df4))
-##print (type(df3[col1][3]))
-##print (df3[col1][3])
-##print (type(float(df3[col1][3])))
-##ax.scatter([np.float64(df3[col1][x]) for x in range(len(dflist))], [np.float64(df4[col2][y]) for y in range(len(dflist))])
-
-plt.show()
+createScatter("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "c2: Heating:DistrictHeating [J](RunPeriod)", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv", "t2: Cooling [kWh]", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")

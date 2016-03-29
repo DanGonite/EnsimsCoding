08e790c951095332ca2782066a88aa5192301d9c
Dan <DanZinc57@gmail.com>
2016-03-29 18:14:09 +0100

    Works

--- SimResultsScatterPy3v3.py
+++ SimResultsScatterPy3v3.py
@@ -13,33 +13,26 @@ def indexSelection(ifile1, ifile2):
         i += 1
     for item in headers:
         print(item, headers[item])
+    total = pd.concat([df1,df2], axis=1)
+    total.to_csv("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/TestCSV.csv")
 
 def createScatter(col1, col2, ofolder):
     val1 = headers[col1]
     val2 = headers[col2]
     df1 = total[val1]
     df2 = total[val2]
-
     f, ax = plt.subplots()
     i = 0
     for item in df1:
-        print(i)
         try:
             ax.scatter(df1[i], df2[i])
         except KeyError:
             break
         i += 1
-##
-##    col1name = val1.replace(":", "")
-##    col2name = val2.replace(":", "")
-##    fname = col1name + " - " + col2name
-##    ax.set_title(fname)
-##    ax.figure.savefig(ofolder+"/"+fname+".png", format="png")
-
     plt.show()
 
 indexSelection("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv")
+createScatter(24, 23, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
 
-createScatter(23, 27, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
-
-##createScatter(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+##indexSelection(sys.argv[1], sys.argv[2])
+##createScatter(sys.argv[1], sys.argv[2])

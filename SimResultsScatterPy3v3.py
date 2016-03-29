b9b0403f9f90c804b6c14c3cccd89454cdaeca24
Dan <DanZinc57@gmail.com>
2016-03-29 12:53:28 +0100

    Added user-friendliness

--- SimResultsScatterPy3v3.py
+++ SimResultsScatterPy3v3.py
@@ -1,26 +1,45 @@
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-def createScatter(ifile1, col1, ifile2, col2, ofolder):
-    df1 = pd.read_csv(ifile1)
-    df2 = pd.read_csv(ifile2)
+def indexSelection(ifile1, ifile2):
+    df1 = pd.read_csv(ifile1, skipinitialspace=True)
+    df2 = pd.read_csv(ifile2, skipinitialspace=True)
+    global total
     total = pd.concat([df1,df2])
-##    f, ax = plt.subplots()
-##    df3 = df1[col1]
-##    df4 = df2[col2]
+    global headers
+    headers = {}
+    i = 0
+    for item in total:
+        headers[i] = item
+        i += 1
+    for item in headers:
+        print(item, headers[item])
+
+def createScatter(col1, col2, ofolder):
+    val1 = headers[col1]
+    val2 = headers[col2]
+    df1 = total[val1]
+    df2 = total[val2]
+
+    f, ax = plt.subplots()
+    i = 0
+    for item in df1:
+        print(i)
+        try:
+            ax.scatter(df1[i], df2[i])
+        except KeyError:
+            break
+        i += 1
 ##
-##    i = 0
-##    for item in df3:
-##        ax.scatter(df3[i], df4[i])
-##        i += 1
-##
-##    col1name = col1.replace(":", "")
-##    col2name = col2.replace(":", "")
+##    col1name = val1.replace(":", "")
+##    col2name = val2.replace(":", "")
 ##    fname = col1name + " - " + col2name
 ##    ax.set_title(fname)
 ##    ax.figure.savefig(ofolder+"/"+fname+".png", format="png")
-##
-##    plt.show()
 
-createScatter("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "c1: InteriorEquipment:Electricity [J](RunPeriod)", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv", "t2: Cooling [kWh]", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
+    plt.show()
+
+indexSelection("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv")
+
+createScatter(23, 27, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
 
 ##createScatter(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])

32bb450777fc3e453ca1a76c4398d6bc348a1de0
Dan <DanZinc57@gmail.com>
2016-03-30 09:50:31 +0100

    Should work

--- SimResultsScatterPy3v3.py
+++ SimResultsScatterPy3v3.py
@@ -3,8 +3,13 @@ import numpy as np, pandas as pd, matplotlib.pyplot as plt
 def indexSelection(ifile1, ifile2):
     df1 = pd.read_csv(ifile1, skipinitialspace=True)
     df2 = pd.read_csv(ifile2, skipinitialspace=True)
+    del df1["#"]
+    del df2["#"]
+    df1 = df1.sort(["Job_ID"], ascending=[True])
+    df2 = df2.sort(["Job_ID"], ascending=[True])
+    del df2["Job_ID"]
     global total
-    total = pd.concat([df1,df2])
+    total = pd.concat([df1,df2]).dropna(axis=1, how="all")
     global headers
     headers = {}
     i = 0
@@ -14,6 +19,8 @@ def indexSelection(ifile1, ifile2):
     for item in headers:
         print(item, headers[item])
     total = pd.concat([df1,df2], axis=1)
+    df1.to_csv("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/df1CSV.csv")
+    df2.to_csv("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/df2CSV.csv")
     total.to_csv("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/TestCSV.csv")
 
 def createScatter(col1, col2, ofolder):
@@ -31,8 +38,8 @@ def createScatter(col1, col2, ofolder):
         i += 1
     plt.show()
 
-indexSelection("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv")
-createScatter(24, 23, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
+##indexSelection("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv")
+##createScatter(22, 25, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
 
-##indexSelection(sys.argv[1], sys.argv[2])
-##createScatter(sys.argv[1], sys.argv[2])
+indexSelection(sys.argv[1], sys.argv[2])
+createScatter(sys.argv[1], sys.argv[2], sys.argv[3])

715bd96b40cd0e97a542f77e17557c83ad9459f1
Dan <DanZinc57@gmail.com>
2016-04-01 17:51:06 +0100

    Merging

--- SimResultsScatterPy3.py
+++ SimResultsScatterPy3.py
@@ -1,29 +1,40 @@
-import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, sys
 
-def createScatter(ifile1, col1, ifile2, col2, ofolder):
-    df1 = pd.read_csv(ifile1)
-    df2 = pd.read_csv(ifile2)
-    f, ax = plt.subplots()
-    df3 = df1[col1]
-    df4 = df2[col2]
-    print (type(df3))
-
-    i = 0
-    for item in df3:
-        print (df3[i])
-        print (df4[i])
-        print ("")
-        ax.scatter(df3[i], df4[i])
+def indexSelection(ifile1, ifile2):
+    df1 = pd.read_csv(ifile1, skipinitialspace=True)
+    df2 = pd.read_csv(ifile2, skipinitialspace=True)
+    for i in range(2):
         i += 1
+        del locals()["df"+str(i)]["#"]
+        locals()["df"+str(i)] = locals()["df"+str(i)].sort(["Job_ID"], ascending=[True])
+    del df2["Job_ID"]
+    global total
+    total = pd.concat([df1,df2]).dropna(axis=1, how="all")
+    global headers
+    headers = {}
+    i = 0
+    for item in total:
+        headers[i] = item
+        i += 1
+    for item in headers:
+        print(item, headers[item])
+    total = pd.concat([df1,df2], axis=1)
 
-    col1name = col1.replace(":", "")
-    col2name = col2.replace(":", "")
-    fname = col1name + " - " + col2name
-    ax.set_title(fname)
-    ax.figure.savefig(ofolder+"/"+fname+".png", format="png")
-
+def createScatter(col1, col2, ofolder):
+    df1 = total[headers[col1]]
+    df2 = total[headers[col2]]
+    f, ax = plt.subplots()
+    i = 0
+    for item in df1:
+        try:
+            ax.scatter(df1[i], df2[i])
+        except KeyError:
+            break
+        i += 1
     plt.show()
 
-createScatter("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "c2: Heating:DistrictHeating [J](RunPeriod)", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv", "t1: Heating [kWh]", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
+indexSelection("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv")
+createScatter(21, 22, "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
 
-##createScatter(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+#indexSelection(sys.argv[1], sys.argv[2])
+#createScatter(sys.argv[1], sys.argv[2], sys.argv[3])

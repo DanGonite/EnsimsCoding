9894c9fde3aba601cb1529c58501857618a4d9e5
Dan <DanZinc57@gmail.com>
2016-04-01 17:50:39 +0100

    Merging

--- SimResultsScatterPy3.py
+++ SimResultsScatterPy3.py
@@ -1,11 +1,29 @@
-import pandas as pd, matplotlib.pyplot as plt
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-def createScatterPlot(ifile1, ifile2, x, y):
+def createScatter(ifile1, col1, ifile2, col2, ofolder):
     df1 = pd.read_csv(ifile1)
     df2 = pd.read_csv(ifile2)
-    dfMaster = pd.concat([df1, df2])
-    #x = x for x in 
-    plt.scatter(x,y, s=10)
+    f, ax = plt.subplots()
+    df3 = df1[col1]
+    df4 = df2[col2]
+    print (type(df3))
+
+    i = 0
+    for item in df3:
+        print (df3[i])
+        print (df4[i])
+        print ("")
+        ax.scatter(df3[i], df4[i])
+        i += 1
+
+    col1name = col1.replace(":", "")
+    col2name = col2.replace(":", "")
+    fname = col1name + " - " + col2name
+    ax.set_title(fname)
+    ax.figure.savefig(ofolder+"/"+fname+".png", format="png")
+
     plt.show()
-    
-createScatterPlot("C:/Users/Jurong/EnsimsCoding/run/AllDerivedResults.csv","C:/Users/Jurong/EnsimsCoding/run/AllCombinedResults.csv",10,10)
+
+createScatter("D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllCombinedResults.csv", "c2: Heating:DistrictHeating [J](RunPeriod)", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run/AllDerivedResults.csv", "t1: Heating [kWh]", "D:/USB/Total Backup/Things/Programming/EnsimsCoding/run")
+
+##createScatter(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
